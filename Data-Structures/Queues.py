f28c4a2ced5b4b8badbeacba4cb366763485e127
Islombek Nematov <78846913+islombeknematov@users.noreply.github.com>
2022-04-20 10:15:47 +0500

    Update Queues.py

--- Data-Structures/Queues.py
+++ Data-Structures/Queues.py
@@ -1,23 +1,23 @@
-# QUEUES
+QUEUES
 
-                        # 1.1 Theory
+                        1.1 Theory
 
-# A queue is a FIFO (First In First Out). This structure is
-# named as "queue" because it resembles a real-world queue
-# people waiting in a queue
+A queue is a FIFO (First In First Out). This structure is
+named as "queue" because it resembles a real-world queue
+people waiting in a queue
 
-# https://miro.medium.com/max/630/1*GbtPRh9OWh1jtCtCa9czIg.jpeg
-
-
-                        # 1.2 Queue Operations
-
-# 1) ENQUEUE - Insert an element to the end of the queue
-# 2) DEQUEUE - Delete the element from the beginning of the queue
-
-# https://miro.medium.com/max/630/1*K4-7c0lyUcSGRPmv3_9uqw.png
+https://miro.medium.com/max/630/1*GbtPRh9OWh1jtCtCa9czIg.jpeg
 
 
-                        # 1.3 Applications of queues
+                        1.2 Queue Operations
 
-# - Used to manage threads in multithreading.
-#  - Used to implement queuing systems (e.g.: priority queues).
+1) ENQUEUE - Insert an element to the end of the queue
+2) DEQUEUE - Delete the element from the beginning of the queue
+
+https://miro.medium.com/max/630/1*K4-7c0lyUcSGRPmv3_9uqw.png
+
+
+                        1.3 Applications of queues
+
+- Used to manage threads in multithreading.
+ - Used to implement queuing systems (e.g.: priority queues).
