5a17484abdbec7241313dc06c9553cf6beaa4531
Islombek Nematov <78846913+islombeknematov@users.noreply.github.com>
2022-04-20 10:16:11 +0500

    Update Hash_tables.py

--- Data-Structures/Hash_tables.py
+++ Data-Structures/Hash_tables.py
@@ -1,10 +1,10 @@
 
-# HASH TABLES
+HASH TABLES
 
-# A Hash Table is a data structure that stores values which
-# have keys associated with each of them. Furthermore, it
-# supports lookup efficiently if we know the key associated
-# with the value. Hence it is very efficient in inserting
-# and searching, irrespective of the size of the data.
+A Hash Table is a data structure that stores values which
+have keys associated with each of them. Furthermore, it
+supports lookup efficiently if we know the key associated
+with the value. Hence it is very efficient in inserting
+and searching, irrespective of the size of the data.
 
 
