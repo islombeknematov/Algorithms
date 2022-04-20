f0586013d621569bec3d198ff9824531d97a54cb
Islombek Nematov <78846913+islombeknematov@users.noreply.github.com>
2022-04-20 10:16:30 +0500

    Update Arrays.py

--- Data-Structures/Arrays.py
+++ Data-Structures/Arrays.py
@@ -1,37 +1,37 @@
-# ARRAYS
+ARRAYS
 
-#                       1.1 Theory
+                      1.1 Theory
 
-# An array is a structure of fixed-size, which can hold items of the
-# same data type. It can be an array of integers, an array of floating
-# point numbers, an array of strings or even an array of arrays
-# (such as 2-dimensional arrays). Arrays are indexed meaning that
-# random access is possible.
-
-
-#                       1.2 Array Image
-
-# https://miro.medium.com/max/630/1*pYIKtQYbX8vgCWrwe1YOyg.png
+An array is a structure of fixed-size, which can hold items of the
+same data type. It can be an array of integers, an array of floating
+point numbers, an array of strings or even an array of arrays
+(such as 2-dimensional arrays). Arrays are indexed meaning that
+random access is possible.
 
 
-#                       1.3 Array Operations
+                      1.2 Array Image
 
-# - TRAVERSE: Go through the elements and print them
-# - SEARCH: Search for an element in the array. You can
-#       search the element by its value and index
-# - UPDATE: Update the value of an existing element at a given index
-
-# - INSERTING elements to an array and DELETING elements from
-#       an array cannot be done straight away as arrays are
-#       fixed in size. If you want to insert an element to an
-#       array, first you have to create a new array with increased
-#       size (current size + 1), copy the existing elements and add
-#       the nwe element. The same goes for the deletion with a new
-#       array of reduced size.
+https://miro.medium.com/max/630/1*pYIKtQYbX8vgCWrwe1YOyg.png
 
 
-#                       1.4 Applications Of Arrays
-#   - Used as the building blocks to build other data structures
-#       such as array lists, heaps, hash tables, vectors and matrices.
-#   - Used for different sorting algorithms such as insertion sort,
-#       quick sort, bubble sort and merge sort.
+                      1.3 Array Operations
+
+- TRAVERSE: Go through the elements and print them
+- SEARCH: Search for an element in the array. You can
+      search the element by its value and index
+- UPDATE: Update the value of an existing element at a given index
+
+- INSERTING elements to an array and DELETING elements from
+      an array cannot be done straight away as arrays are
+      fixed in size. If you want to insert an element to an
+      array, first you have to create a new array with increased
+      size (current size + 1), copy the existing elements and add
+      the nwe element. The same goes for the deletion with a new
+      array of reduced size.
+
+
+                      1.4 Applications Of Arrays
+  - Used as the building blocks to build other data structures
+      such as array lists, heaps, hash tables, vectors and matrices.
+  - Used for different sorting algorithms such as insertion sort,
+      quick sort, bubble sort and merge sort.
